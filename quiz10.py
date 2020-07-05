bc9a43899688e9f7f3c5e07464f8b3e21ada160a
Yunjimin <67583784+Yunjimin@users.noreply.github.com>
2020-07-05 22:18:50 +0900

    간단문제

---
+++ quiz10.py
@@ -0,0 +1,17 @@
+'''
+ 내에 나만의 시그니처를 남기는 모듈을 만드시오
+
+조건 : 모든 파일명은 byme.py 로 작성
+
+(모듈 사용 예제)
+import byme
+byme.sign()
+
+출력 예제
+이 프로그램은 나도코딩에 의해 만들어졌습니다.
+하하하 : dkfsdfjsd
+호호호 : fskd;jfsd
+'''
+
+import byme
+byme.sign()
